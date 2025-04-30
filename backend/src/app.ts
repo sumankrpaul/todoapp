391a7be34c11e8ea86c6d72382a998629736365f
Suman Kumar Paul <suman_paul@avya-tech.com>
2025-05-01 04:44:01 +0530

    Backend: Prisma configured and Nodemon added for dev

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,33 @@
 import express from "express";
+import { PrismaClient, User } from "@prisma/client";
 const port = process.env.PORT || 4000
 const app = express();
+
+const prisma = new PrismaClient();
+
+const testDB = ()=>{
+    prisma.user.create({
+        data:{
+            name:"Test User ",
+            email: `user${(+(new Date())).toString().substring(8)}@mail.com`
+        }
+    }).then(()=>{
+        console.log("User got created");
+    })
+}
 
 app.get("/", (req, res)=>{
     res.json({ message: "Yea!!!" });
 })
 
-app.listen(port, ()=>{ console.log("app is running at", port) })
+app.get("/users", (req, res)=>{
+    prisma.user.findMany().then((users)=>{
+        res.json({user_list: users});
+    }).catch(error =>{
+        res.json({ message: "Error occured!", error: error })
+    })
+})
+
+app.get("/test", (req,res)=>{ res.json({ "message":"it worked again"}) })
+
+app.listen(port, ()=>{ console.log("app is running at", port); testDB()})
