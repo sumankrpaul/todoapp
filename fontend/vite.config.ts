f1b46b6c29c4de66984c51d3fc708ab4731a76a8
Suman Kumar Paul <suman_paul@avya-tech.com>
2025-05-02 01:31:04 +0530

    Frontend: Changed front-end from react to vue.js

--- fontend/vite.config.ts
+++ fontend/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
+  plugins: [vue()],
+  server:{
     port: 8001,
-    watch: {
+    watch:{
       usePolling: true
     }
   }
